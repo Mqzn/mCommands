23eee29828cf0e3d5fa09de80664a2d188dfe590
Mazen <mezoahmed2507@gmail.com>
2023-05-07 14:56:36 +0300

    Added new features for the new version and fixed some bugs

--- bungee/src/main/java/io/github/mqzn/commands/BungeeCommandManager.java
+++ bungee/src/main/java/io/github/mqzn/commands/BungeeCommandManager.java
@@ -1,9 +1,11 @@
 package io.github.mqzn.commands;
 
+import io.github.mqzn.commands.arguments.ArgumentOnlinePlayer;
 import io.github.mqzn.commands.base.Command;
 import io.github.mqzn.commands.base.manager.AbstractCommandManager;
 import io.github.mqzn.commands.base.manager.CommandExecutionCoordinator;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
@@ -20,6 +22,7 @@ public final class BungeeCommandManager extends AbstractCommandManager<Plugin, C
 		captionRegistry.registerCaption(BungeeCaption.ONLY_PLAYER_EXECUTABLE);
 		captionRegistry.registerCaption(BungeeCaption.NO_HELP_TOPIC_AVAILABLE);
 
+		typeRegistry().registerArgumentConverter(ProxiedPlayer.class, ArgumentOnlinePlayer::new);
 	}
 
 	public BungeeCommandManager(@NotNull Plugin plugin) {
---
+++ bungee/src/main/java/io/github/mqzn/commands/arguments/ArgumentOnlinePlayer.java
@@ -0,0 +1,54 @@
+package io.github.mqzn.commands.arguments;
+
+import io.github.mqzn.commands.exceptions.types.ArgumentParseException;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import org.jetbrains.annotations.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArgumentOnlinePlayer extends AbstractArgument<ProxiedPlayer> {
+
+
+	public ArgumentOnlinePlayer(@NotNull String id) {
+		super(id, ProxiedPlayer.class);
+	}
+
+	public ArgumentOnlinePlayer(@NotNull String id, boolean optional, boolean useRemainingSpace) {
+		super(id, ProxiedPlayer.class, optional, useRemainingSpace);
+	}
+
+	public ArgumentOnlinePlayer(String id, boolean useRemainingSpace) {
+		super(id, ProxiedPlayer.class, useRemainingSpace);
+	}
+
+	public ArgumentOnlinePlayer(@NotNull ArgumentData data) {
+		super(data, ProxiedPlayer.class);
+	}
+
+	@Override
+	public ProxiedPlayer parse(@NotNull String command, @NotNull String input) throws ArgumentParseException {
+
+		ProxiedPlayer player = ProxyServer.getInstance().getPlayer(input);
+		if(player == null || !player.isConnected()) {
+			throw new ArgumentParseException(String.format("Player %s is offline or doesn't exist", input), input, command);
+		}
+
+		return player;
+	}
+
+	@Override
+	public @NotNull List<ProxiedPlayer> suggestions() {
+		return new ArrayList<>(ProxyServer.getInstance().getPlayers());
+	}
+
+	@Override
+	public boolean isSuggestionDynamic() {
+		return true;
+	}
+
+	@Override
+	public String toString(ProxiedPlayer obj) {
+		return obj.getName();
+	}
+}
