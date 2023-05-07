50162bbd2903312ecc29efc71f16ac9c93fe1d45
Mazen <mezoahmed2507@gmail.com>
2023-05-07 14:57:40 +0300

    Added new features for the new version and fixed some bugs

--- bungee/src/main/java/io/github/mqzn/commands/arguments/ArgumentOnlinePlayer.java
+++ bungee/src/main/java/io/github/mqzn/commands/arguments/ArgumentOnlinePlayer.java
@@ -7,7 +7,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ArgumentOnlinePlayer extends AbstractArgument<ProxiedPlayer> {
+public final class ArgumentOnlinePlayer extends AbstractArgument<ProxiedPlayer> {
 
 
 	public ArgumentOnlinePlayer(@NotNull String id) {
